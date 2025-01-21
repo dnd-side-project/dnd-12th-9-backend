fbec3271a7757056782942ddd5689b2246a528ee
hwangdaesun <hds650588@gmail.com>
2025-01-21 21:22:37 +0900

    feat : MemberEntity 생성

---
+++ core/src/main/java/com/dnd/book/member/MemberEntity.java
@@ -0,0 +1,34 @@
+package com.dnd.book.member;
+
+import com.dnd.book.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "member")
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class MemberEntity extends BaseEntity {
+
+    public static final String ENTITY_PREFIX = "member";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = ENTITY_PREFIX + "_nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = ENTITY_PREFIX + "_introduction", nullable = false)
+    private String introduction;
+}
