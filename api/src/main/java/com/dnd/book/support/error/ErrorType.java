f70d448b26086fe1cd7ed75f2e5bc7d83c22225e
seungjo <f1v3@kakao.com>
2025-01-21 16:52:54 +0900

    feat : 공통 예외 구조 세팅

---
+++ api/src/main/java/com/dnd/book/support/error/ErrorType.java
@@ -0,0 +1,30 @@
+package com.dnd.book.support.error;
+
+
+import lombok.Getter;
+import org.springframework.boot.logging.LogLevel;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorType {
+
+    // TODO: 에러 타입 정의 필요
+    DEFAULT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, ErrorCode.E500,
+            "An unexpected error has occurred.", LogLevel.ERROR),
+
+    NOT_FOUND(HttpStatus.NOT_FOUND, ErrorCode.E404,
+            "The requested resource was not found.", LogLevel.INFO);
+
+
+    private final HttpStatus status;
+    private final ErrorCode code;
+    private final String message;
+    private final LogLevel logLevel;
+
+    ErrorType(HttpStatus status, ErrorCode code, String message, LogLevel logLevel) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+        this.logLevel = logLevel;
+    }
+}
