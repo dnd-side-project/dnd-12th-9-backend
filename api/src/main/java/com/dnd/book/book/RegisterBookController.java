829e6a57fd407400ccc0a2b80eb33bf0c2587674
hwangdaesun <hds650588@gmail.com>
2025-01-22 10:52:26 +0900

    feat : ApiResponse 적용

--- api/src/main/java/com/dnd/book/book/RegisterBookController.java
+++ api/src/main/java/com/dnd/book/book/RegisterBookController.java
@@ -1,9 +1,9 @@
 package com.dnd.book.book;
 
 import com.dnd.book.book.request.RegisterBookRequest;
+import com.dnd.book.support.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +17,7 @@ public class RegisterBookController {
     private final RegisterBookUsecase registerService;
 
     @PostMapping("/book")
-    public ResponseEntity<Void> registerBook(@Valid @RequestBody RegisterBookRequest request) {
+    public ApiResponse<?> registerBook(@Valid @RequestBody RegisterBookRequest request) {
         Long memberId = 1L; // 임시
         registerService.registerBook(request, memberId);
         return ResponseEntity.ok().build();
