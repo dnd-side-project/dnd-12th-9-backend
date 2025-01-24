cd839e042a5312d53f82a4897df27724599a08dd
hwangdaesun <hds650588@gmail.com>
2025-01-25 01:33:18 +0900

    feat : securityConfig 설정

---
+++ api/src/main/java/com/dnd/book/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.dnd.book.security;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                .anyRequest().authenticated());
+        http.oauth2Login(withDefaults());
+        return http.build();
+    }
+
+}
+
