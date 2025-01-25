ee575f8916c2103227462f72c09098da57e02fbe
hwangdaesun <hds650588@gmail.com>
2025-01-25 10:06:33 +0900

    clear git cache

--- api/src/main/java/com/dnd/book/security/SecurityConfig.java
+++ api/src/main/java/com/dnd/book/security/SecurityConfig.java
@@ -2,19 +2,27 @@ package com.dnd.book.security;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
-                .anyRequest().authenticated());
+                .anyRequest().permitAll());
         http.oauth2Login(withDefaults());
         return http.build();
     }
