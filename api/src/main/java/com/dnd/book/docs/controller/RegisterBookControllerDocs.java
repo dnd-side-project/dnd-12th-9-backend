073d0d4c66611e50c88b36fb1ca7c0448fcf143e
hwangdaesun <hds650588@gmail.com>
2025-01-27 17:40:45 +0900

    feat : @AuthenticationPrincipal 적용

--- api/src/main/java/com/dnd/book/docs/controller/RegisterBookControllerDocs.java
+++ api/src/main/java/com/dnd/book/docs/controller/RegisterBookControllerDocs.java
@@ -1,9 +1,11 @@
 package com.dnd.book.docs.controller;
 
 import com.dnd.book.book.request.RegisterBookRequest;
+import com.dnd.book.member.MemberEntity;
 import com.dnd.book.support.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Tag(
         name = "[Book API]",
@@ -13,6 +15,6 @@ public interface RegisterBookControllerDocs {
 
     @Operation(summary = "책 등록", description = "회원이 새로운 책을 등록한다.")
 
-    ApiResponse<?> registerBook(RegisterBookRequest request);
+    ApiResponse<?> registerBook(RegisterBookRequest request, UserDetails userDetails);
 
 }
