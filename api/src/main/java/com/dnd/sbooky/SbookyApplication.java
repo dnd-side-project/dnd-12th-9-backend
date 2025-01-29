cdb755b5fb604e20743c9db5e020ad37d24671f6
seungjo <f1v3@kakao.com>
2025-01-29 18:06:37 +0900

    chore : Application 명 수정

--- api/src/main/java/com/dnd/sbooky/SbookyApplication.java
+++ api/src/main/java/com/dnd/sbooky/SbookyApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class BookApplication {
+public class SbookyApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BookApplication.class, args);
+        SpringApplication.run(SbookyApplication.class, args);
     }
 }
