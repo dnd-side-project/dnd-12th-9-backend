ad97a263b722bfc5399d9139da3c5e4f033a97a4
hwangdaesun <hds650588@gmail.com>
2025-01-30 14:21:26 +0900

    refactor : setAccessTokenHeader 메서드 추출

--- api/src/main/java/com/dnd/sbooky/api/security/OAuth2SuccessHandler.java
+++ api/src/main/java/com/dnd/sbooky/api/security/OAuth2SuccessHandler.java
@@ -26,6 +26,12 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
                                         Authentication authentication) throws IOException{
 
         String accessToken = tokenProvider.generateAccessToken(authentication);
+        setAccessTokenHeader(response, accessToken);
+        objectMapper.writeValue(response.getWriter(),
+                new ApiResponse(ResultType.SUCCESS, null, null));
+    }
+
+    private void setAccessTokenHeader(HttpServletResponse response, String accessToken) {
         StringBuilder sb = new StringBuilder();
         sb.append(TokenConstants.TOKEN_PREFIX).append(accessToken);
         response.setHeader(AUTHORIZATION, sb.toString());
@@ -33,7 +39,5 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setCharacterEncoding("UTF-8");
         response.isCommitted();
-        objectMapper.writeValue(response.getWriter(),
-                new ApiResponse(ResultType.SUCCESS, null, null));
     }
 }
