d3335fd8a8e1aa01d55e68a8543b8e9d5557c6a6
hwangdaesun <hds650588@gmail.com>
2025-01-30 23:44:01 +0900

    feat : 토큰 재발급 API swagger에 추가

---
+++ api/src/main/java/com/dnd/sbooky/api/docs/controller/AuthControllerDocs.java
@@ -0,0 +1,16 @@
+package com.dnd.sbooky.api.docs.controller;
+
+import com.dnd.sbooky.api.support.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Tag(
+        name = "[Auth API]",
+        description = "인증에 관련된 API"
+)
+public interface AuthControllerDocs {
+    @Operation(summary = "토큰 재발급", description = "refreshToken 검증 후 이상 없을 시 accessToken과 refreshToken을 재발급한다.")
+    ApiResponse<?> reissue(String refreshToken, HttpServletResponse response);
+
+}
