a7ba4c9a62e87386d54356750a816ba2157ee797
hwangdaesun <hds650588@gmail.com>
2025-01-30 15:27:15 +0900

    refactor : static import 사용

--- api/src/main/java/com/dnd/sbooky/api/security/OAuth2SuccessHandler.java
+++ api/src/main/java/com/dnd/sbooky/api/security/OAuth2SuccessHandler.java
@@ -1,7 +1,8 @@
 package com.dnd.sbooky.api.security;
 
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-
+import static com.dnd.sbooky.api.security.TokenConstants.*;
+import static java.nio.charset.StandardCharsets.*;
+import static org.springframework.http.HttpHeaders.*;
 import com.dnd.sbooky.api.support.response.ApiResponse;
 import com.dnd.sbooky.api.support.response.ResultType;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -33,11 +34,11 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
     private void setAccessTokenHeader(HttpServletResponse response, String accessToken) {
         StringBuilder sb = new StringBuilder();
-        sb.append(TokenConstants.TOKEN_PREFIX).append(accessToken);
+        sb.append(TOKEN_PREFIX).append(accessToken);
         response.setHeader(AUTHORIZATION, sb.toString());
         response.setStatus(HttpServletResponse.SC_OK);
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding(UTF_8.name());
         response.isCommitted();
     }
 }
