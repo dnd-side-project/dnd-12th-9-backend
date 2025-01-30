379ad5f59bcf0a170953dc7eb282cbbaa773502c
hwangdaesun <hds650588@gmail.com>
2025-01-30 21:37:18 +0900

    feat : 로그인 후 토큰 재발급 시 새로운 토큰으로 저장/업데이트

--- api/src/main/java/com/dnd/sbooky/api/security/OAuth2SuccessHandler.java
+++ api/src/main/java/com/dnd/sbooky/api/security/OAuth2SuccessHandler.java
@@ -5,6 +5,8 @@ import static java.nio.charset.StandardCharsets.*;
 import static org.springframework.http.HttpHeaders.*;
 import com.dnd.sbooky.api.support.response.ApiResponse;
 import com.dnd.sbooky.api.support.response.ResultType;
+import com.dnd.sbooky.api.support.RedisKey;
+import com.dnd.sbooky.core.RedisRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -22,6 +24,7 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
     private final TokenProvider tokenProvider;
     private final ObjectMapper objectMapper;
+    private final RedisRepository redisRepository;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -31,8 +34,14 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         setAccessTokenHeader(response, accessToken);
         String refreshToken = tokenProvider.generateRefreshToken(authentication);
         setRefreshTokenCookie(response, refreshToken);
+        redisRepository.setData(getKey(authentication), refreshToken, REFRESH_TOKEN_EXPIRE_TIME);
         objectMapper.writeValue(response.getWriter(),
                 new ApiResponse(ResultType.SUCCESS, null, null));
+    }
+
+    private String getKey(Authentication authentication) {
+        StringBuilder sb = new StringBuilder();
+        return sb.append(RedisKey.refreshTokenPrefix).append(authentication.getName()).toString();
     }
 
     private void setAccessTokenHeader(HttpServletResponse response, String accessToken) {
