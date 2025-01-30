5829cb9b8284f19aac578c4b0b8a036670838dab
hwangdaesun <hds650588@gmail.com>
2025-01-30 21:35:34 +0900

    feat : RedisRepository 생성

--- core/src/main/java/com/dnd/sbooky/core/config/RedisConfig.java
+++ core/src/main/java/com/dnd/sbooky/core/config/RedisConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -27,5 +29,13 @@ public class RedisConfig {
         LettuceConnectionFactory lettuceConnectionFactory =
                 new LettuceConnectionFactory(redisConfiguration);
         return lettuceConnectionFactory;
+    }
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
     }
 }
