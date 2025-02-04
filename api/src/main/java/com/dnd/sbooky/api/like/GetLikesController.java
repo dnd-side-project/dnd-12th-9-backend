41004df48854f6517249b14a66f7e1a33ee31b21
hwangdaesun <hds650588@gmail.com>
2025-02-04 21:59:11 +0900

    feat : 호스트용 좋아요 수 조회 API 구현

--- api/src/main/java/com/dnd/sbooky/api/like/GetLikesController.java
+++ api/src/main/java/com/dnd/sbooky/api/like/GetLikesController.java
@@ -3,7 +3,10 @@ package com.dnd.sbooky.api.like;
 import com.dnd.sbooky.api.docs.spec.GetLikesApiSpec;
 import com.dnd.sbooky.api.like.response.GetLikesResponse;
 import com.dnd.sbooky.api.support.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +22,12 @@ public class GetLikesController implements GetLikesApiSpec {
     @GetMapping("/likes/{memberId}")
     public ApiResponse<?> getLikes(@PathVariable Long memberId) {
         return ApiResponse.success(GetLikesResponse.from(getLikesUsecase.get(memberId)));
+    }
+
+    @GetMapping("/likes")
+    public ApiResponse<?> getLikes(
+            @Parameter(hidden = true) @AuthenticationPrincipal UserDetails user) {
+        return ApiResponse.success(
+                GetLikesResponse.from(getLikesUsecase.get(Long.valueOf(user.getUsername()))));
     }
 }
