cb3da7b6b4ae580cbe378741fdc3a9d924fd29f2
hwangdaesun <hds650588@gmail.com>
2025-02-04 17:43:27 +0900

    refactor : @RequiredArgsConstructor 적용

--- core/src/main/java/com/dnd/sbooky/core/LockTimeoutRepository.java
+++ core/src/main/java/com/dnd/sbooky/core/LockTimeoutRepository.java
@@ -1,20 +1,16 @@
 package com.dnd.sbooky.core;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class LockTimeoutRepository {
 
     private final JdbcTemplate jdbcTemplate;
-
-    public LockTimeoutRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
 
     public void setInnodbLockWaitTimeout(int seconds) {
         jdbcTemplate.execute("SET SESSION lock_wait_timeout = " + seconds);
     }
 }
-
-
