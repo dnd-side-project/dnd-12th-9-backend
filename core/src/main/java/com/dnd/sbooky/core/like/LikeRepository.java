dc72057d734d7ab792d308b5aa6b0979409ba3de
hwangdaesun <hds650588@gmail.com>
2025-02-04 17:25:35 +0900

    feat : findByIdWithPessimisticLock메서드에 @LockTimeout 적용

--- core/src/main/java/com/dnd/sbooky/core/like/LikeRepository.java
+++ core/src/main/java/com/dnd/sbooky/core/like/LikeRepository.java
@@ -1,5 +1,6 @@
 package com.dnd.sbooky.core.like;
 
+import com.dnd.sbooky.core.LockTimeout;
 import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface LikeRepository extends JpaRepository<LikeEntity, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @LockTimeout
     @Query("select l from LikeEntity l where l.id = :id")
     Optional<LikeEntity> findByIdWithPessimisticLock(Long id);
 }
