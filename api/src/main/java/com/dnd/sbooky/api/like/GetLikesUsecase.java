d038e524a3c2521feb1679e9d03aa4fb641be7fd
hwangdaesun <hds650588@gmail.com>
2025-02-04 21:30:04 +0900

    feat : 좋아요 수를 조회하는 유스케이스 구현

---
+++ api/src/main/java/com/dnd/sbooky/api/like/GetLikesUsecase.java
@@ -0,0 +1,24 @@
+package com.dnd.sbooky.api.like;
+
+import com.dnd.sbooky.api.book.exception.MemberNotFoundException;
+import com.dnd.sbooky.api.support.error.ErrorType;
+import com.dnd.sbooky.core.like.LikeEntity;
+import com.dnd.sbooky.core.like.LikeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class GetLikesUsecase {
+    private final LikeRepository likeRepository;
+
+    @Transactional(readOnly = true)
+    public Long get(Long memberId) {
+        LikeEntity likeEntity =
+                likeRepository
+                        .findById(memberId)
+                        .orElseThrow(() -> new MemberNotFoundException(ErrorType.MEMBER_NOT_FOUND));
+        return likeEntity.getCount();
+    }
+}
