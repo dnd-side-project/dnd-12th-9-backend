c4b3204048e6a15bc153bee1dd93c6d12706cf10
hwangdaesun <hds650588@gmail.com>
2025-02-04 23:37:17 +0900

    fix : 좋아요 수 조회 API 통합하여 사용하도록 수정

--- api/src/main/java/com/dnd/sbooky/api/docs/spec/GetLikesApiSpec.java
+++ api/src/main/java/com/dnd/sbooky/api/docs/spec/GetLikesApiSpec.java
@@ -2,16 +2,10 @@ package com.dnd.sbooky.api.docs.spec;
 
 import com.dnd.sbooky.api.support.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.security.core.userdetails.UserDetails;
 
 @Tag(name = "[Like API]", description = "좋아요에 관련된 API")
 public interface GetLikesApiSpec {
-    @Operation(summary = "방문자용 좋아요 수 조회", description = "방문자가 회원의 좋아요 수를 조회한다.")
+    @Operation(summary = "좋아요 수 조회", description = "회원의 좋아요 수를 조회한다.")
     ApiResponse<?> getLikes(Long memberId);
-
-    @SecurityRequirement(name = "access-token")
-    @Operation(summary = "호스타용 좋아요 수 조회", description = "호스트가 회원의 좋아요 수를 조회한다.")
-    ApiResponse<?> getLikes(UserDetails user);
 }
