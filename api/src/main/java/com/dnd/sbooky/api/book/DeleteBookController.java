931305b690c5c031f49ae21e1e2ed267bf83f373
seungjo <f1v3@kakao.com>
2025-02-05 15:00:27 +0900

    feat : 책 삭제 API

---
+++ api/src/main/java/com/dnd/sbooky/api/book/DeleteBookController.java
@@ -0,0 +1,34 @@
+package com.dnd.sbooky.api.book;
+
+import com.dnd.sbooky.api.support.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Parameter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+// todo : add swagger doc
+public class DeleteBookController {
+
+    private final DeleteBookUseCase deleteBookUseCase;
+
+    @DeleteMapping("/books/{memberBookId}")
+    public ApiResponse<?> deleteBook(
+            @PathVariable Long memberBookId,
+            @Parameter(hidden = true) @AuthenticationPrincipal UserDetails user) {
+
+        deleteBookUseCase.delete(extractMemberId(user), memberBookId);
+        return ApiResponse.success();
+    }
+
+    private Long extractMemberId(UserDetails user) {
+        return Long.parseLong(user.getUsername());
+    }
+}
