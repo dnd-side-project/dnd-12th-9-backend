570ba2e51a44be8fdbbdddcd0bf96a7aa3ec137b
seungjo <f1v3@kakao.com>
2025-02-06 00:06:55 +0900

    refactor : HttpStatus static import

--- api/src/main/java/com/dnd/sbooky/api/support/error/ErrorType.java
+++ api/src/main/java/com/dnd/sbooky/api/support/error/ErrorType.java
@@ -1,5 +1,7 @@
 package com.dnd.sbooky.api.support.error;
 
+import static org.springframework.http.HttpStatus.*;
+
 import lombok.Getter;
 import org.springframework.boot.logging.LogLevel;
 import org.springframework.http.HttpStatus;
@@ -7,36 +9,27 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorType {
 
-    // TODO: 에러 타입 정의 필요
-    DEFAULT_ERROR(
-            HttpStatus.INTERNAL_SERVER_ERROR,
-            ErrorCode.E500,
-            "An unexpected error has occurred.",
-            LogLevel.ERROR),
+    // TODO: 에러 타입 정의
 
-    NOT_FOUND(
-            HttpStatus.NOT_FOUND, ErrorCode.E404, "The requested resource was not found.", LogLevel.INFO),
+    // Server Error
+    DEFAULT_ERROR(INTERNAL_SERVER_ERROR, ErrorCode.E500, "Internal server error.", LogLevel.ERROR),
+    INVALID_PARAMETER(BAD_REQUEST, ErrorCode.E400, "Request parameter is invalid.", LogLevel.INFO),
 
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, ErrorCode.SECURITY_401_1, "Invalid token.", LogLevel.INFO),
+    // Security Error
+    INVALID_TOKEN(UNAUTHORIZED, ErrorCode.SECURITY_401_1, "Invalid token.", LogLevel.INFO),
+    INVALID_SIGNATURE(UNAUTHORIZED, ErrorCode.SECURITY_401_2, "Invalid signature", LogLevel.INFO),
+    EXPIRED_TOKEN(UNAUTHORIZED, ErrorCode.SECURITY_401_3, "Expired accessToken", LogLevel.INFO),
+    NOT_FOUND_TOKEN(NOT_FOUND, ErrorCode.SECURITY_404_1, "Not found token", LogLevel.INFO),
 
-    INVALID_SIGNATURE(
-            HttpStatus.UNAUTHORIZED, ErrorCode.SECURITY_401_2, "Invalid signature", LogLevel.INFO),
+    // Member Error
+    MEMBER_NOT_FOUND(NOT_FOUND, ErrorCode.MEMBER_404, "Member not found.", LogLevel.INFO),
 
-    EXPIRED_TOKEN(
-            HttpStatus.UNAUTHORIZED, ErrorCode.SECURITY_401_3, "Expired accessToken", LogLevel.INFO),
+    // Book Error
+    BOOK_ACCESS_FORBIDDEN(FORBIDDEN, ErrorCode.BOOK_403, "Book access is forbidden.", LogLevel.INFO),
+    BOOK_NOT_FOUND(NOT_FOUND, ErrorCode.BOOK_404, "Book was not found.", LogLevel.INFO),
 
-    NOT_FOUND_TOKEN(HttpStatus.NOT_FOUND, ErrorCode.SECURITY_404_1, "Not found token", LogLevel.INFO),
-
-    INVALID_PARAMETER(
-            HttpStatus.BAD_REQUEST, ErrorCode.E400, "Request parameter is invalid.", LogLevel.INFO),
-
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, ErrorCode.MEMBER_404, "Member not found.", LogLevel.INFO),
-
-    BOOK_ACCESS_FORBIDDEN(
-            HttpStatus.FORBIDDEN, ErrorCode.BOOK_403, "Book access is forbidden.", LogLevel.INFO),
-
-    BOOK_NOT_FOUND(HttpStatus.NOT_FOUND, ErrorCode.BOOK_404, "Book was not found.", LogLevel.INFO),
-    ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, ErrorCode.ITEM_404, "Item was not found.", LogLevel.INFO);
+    // Item Error
+    ITEM_NOT_FOUND(NOT_FOUND, ErrorCode.ITEM_404, "Item was not found.", LogLevel.INFO);
 
     private final HttpStatus status;
     private final ErrorCode code;
