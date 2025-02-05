4f56c1e83c7d3199d01aa476d0485aeb2f157b11
hwangdaesun <hds650588@gmail.com>
2025-02-05 11:33:56 +0900

    feat : Initializer에 Item 추가

--- core/src/main/java/com/dnd/sbooky/core/Initializer.java
+++ core/src/main/java/com/dnd/sbooky/core/Initializer.java
@@ -1,5 +1,8 @@
 package com.dnd.sbooky.core;
 
+import com.dnd.sbooky.core.item.ItemEntity;
+import com.dnd.sbooky.core.item.ItemRepository;
+import com.dnd.sbooky.core.item.ItemType;
 import com.dnd.sbooky.core.like.LikeEntity;
 import com.dnd.sbooky.core.like.LikeRepository;
 import com.dnd.sbooky.core.member.MemberEntity;
@@ -10,15 +13,18 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class MemberInitializer {
+public class Initializer {
 
     private final MemberRepository memberRepository;
     private final LikeRepository likeRepository;
+    private final ItemRepository itemRepository;
 
     @PostConstruct
     public void init() {
         MemberEntity memberEntity =
                 memberRepository.save(MemberEntity.newInstance("test1", "test1"));
         likeRepository.save(LikeEntity.newInstance(memberEntity.getId()));
+        itemRepository.save(ItemEntity.newInstance(ItemType.CHARACTER));
+        itemRepository.save(ItemEntity.newInstance(ItemType.CHARACTER));
     }
 }
