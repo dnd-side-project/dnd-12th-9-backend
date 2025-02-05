ef7efc0956baada2420efb99175d5d6476195844
hwangdaesun <hds650588@gmail.com>
2025-02-05 21:50:25 +0900

    feat : 내 아이템을 조회하는 API 응답값 수정

--- api/src/main/java/com/dnd/sbooky/api/item/FindItemsUseCase.java
+++ api/src/main/java/com/dnd/sbooky/api/item/FindItemsUseCase.java
@@ -1,5 +1,6 @@
 package com.dnd.sbooky.api.item;
 
+import com.dnd.sbooky.api.item.response.FindItemResponse;
 import com.dnd.sbooky.api.item.response.FindItemsResponse;
 import com.dnd.sbooky.core.item.ItemType;
 import com.dnd.sbooky.core.item.MemberItemRepository;
@@ -15,16 +16,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class FindItemsUseCase {
 
     private final MemberItemRepository memberItemRepository;
-
     @Transactional(readOnly = true)
     public FindItemsResponse findMyItems(Long memberId) {
-        Map<ItemType, List<Long>> items =
+        Map<ItemType, List<FindItemResponse>> items =
                 memberItemRepository.findItemsByMemberId(memberId).stream()
                         .collect(
                                 Collectors.groupingBy(
-                                        findItemDTO -> findItemDTO.itemType(),
+                                        findItemDTO -> findItemDTO.type(),
                                         Collectors.mapping(
-                                                findItemDTO -> findItemDTO.itemId(),
+                                                findItemDTO ->
+                                                        FindItemResponse.from(
+                                                                findItemDTO.name(),
+                                                                findItemDTO.code()),
                                                 Collectors.toList())));
         return FindItemsResponse.from(items);
     }
