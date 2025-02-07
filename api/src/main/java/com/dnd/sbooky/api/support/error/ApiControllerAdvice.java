eb079e0b4228a21614a68519ccb2d915f7eb979b
seungjo <f1v3@kakao.com>
2025-02-07 16:41:43 +0900

    feat : RequestBody 예외 처리

--- api/src/main/java/com/dnd/sbooky/api/support/error/ApiControllerAdvice.java
+++ api/src/main/java/com/dnd/sbooky/api/support/error/ApiControllerAdvice.java
@@ -1,8 +1,11 @@
 package com.dnd.sbooky.api.support.error;
 
 import com.dnd.sbooky.api.support.response.ApiResponse;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestCookieException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,6 +26,20 @@ public class ApiControllerAdvice {
 
         return ResponseEntity.status(e.getErrorType().getStatus())
                 .body(ApiResponse.error(e.getErrorType(), e.getData()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<?>> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        log.info("MethodArgumentNotValidException = {}", e.getMessage());
+
+        List<String> errorMessages =
+                e.getBindingResult().getFieldErrors().stream()
+                        .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                        .toList();
+
+        return ResponseEntity.status(ErrorType.REQUEST_VALIDATION_FAILED.getStatus())
+                .body(ApiResponse.error(ErrorType.REQUEST_VALIDATION_FAILED, errorMessages));
     }
 
     @ExceptionHandler(MissingRequestCookieException.class)
--- api/src/main/java/com/dnd/sbooky/api/support/error/ErrorCode.java
+++ api/src/main/java/com/dnd/sbooky/api/support/error/ErrorCode.java
@@ -3,17 +3,32 @@ package com.dnd.sbooky.api.support.error;
 public enum ErrorCode {
 
     // TODO: 에러 코드 추가 필요 (Domain + HTTP Status + [Number])
+
+    // Server Error
     E500,
-    E400,
+    E400_1,
+    E400_2,
     E403,
     E404,
+
+    // Member Error
+    MEMBER_404,
+
+    // Book Error
     BOOK_400_1,
     BOOK_403,
     BOOK_404,
-    MEMBER_404,
+
+    // Security Error
     SECURITY_401_2,
     SECURITY_401_3,
     SECURITY_404_1,
+    SECURITY_401_1,
+
+    // Item Error
     ITEM_404,
-    SECURITY_401_1
+
+    // Evaluation Error
+    EVALUATION_404_1,
+    ;
 }
