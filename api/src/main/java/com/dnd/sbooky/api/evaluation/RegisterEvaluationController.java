d30c43cdf703d9439e8656e12a375d2da24d1d85
seungjo <f1v3@kakao.com>
2025-02-09 16:58:26 +0900

    feat : 응답 상태 코드 변경 (201)

--- api/src/main/java/com/dnd/sbooky/api/evaluation/RegisterEvaluationController.java
+++ api/src/main/java/com/dnd/sbooky/api/evaluation/RegisterEvaluationController.java
@@ -6,12 +6,14 @@ import com.dnd.sbooky.api.support.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,6 +24,7 @@ public class RegisterEvaluationController implements RegisterEvaluationApiSpec {
     private final RegisterEvaluationUseCase registerEvaluationUseCase;
 
     @PutMapping("/books/{memberBookId}/evaluation")
+    @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<?> registerEvaluation(
             @PathVariable Long memberBookId,
             @Valid @RequestBody RegisterEvaluationRequest request,
