07b7d251f54d46fff9f03d18b103bb918e5e7d1f
seungjo <f1v3@kakao.com>
2025-02-11 22:44:03 +0900

    refactor : 포인트 정책에서 하나의 포인트 필드로 관리하도록 변경

--- core/src/main/java/com/dnd/sbooky/core/point/PointPolicy.java
+++ core/src/main/java/com/dnd/sbooky/core/point/PointPolicy.java
@@ -6,36 +6,23 @@ import lombok.Getter;
 public enum PointPolicy {
 
     // [100번대] 책 관련 포인트 정책
-    REGISTER_BOOK(100, 10, 0, "책 등록"),
-    COMPLETE_BOOK(101, 50, 0, "책 완독"),
+    REGISTER_BOOK(100, 10, "책 등록"),
+    COMPLETE_BOOK(101, 50, "책 완독"),
 
     // [200번대] 미션 관련 포인트 정책
-    SHARE_BOOK_CARD(200, 50, 0, "독서 카드 공유"),
+    SHARE_BOOK_CARD(200, 50, "독서 카드 공유"),
 
     // [500번대] 아이템 관련 포인트 정책
-    DRAW_ITEM(500, 0, -100, "아이템 뽑기"),
+    DRAW_ITEM(500, -100, "아이템 뽑기"),
     ;
 
     private final Integer id;
-    private final int plus;
-    private final int minus;
+    private final int point;
     private final String description;
 
-    PointPolicy(int id, Integer plus, Integer minus, String description) {
+    PointPolicy(Integer id, int point, String description) {
         this.id = id;
-        this.plus = plus;
-        this.minus = minus;
+        this.point = point;
         this.description = description;
-    }
-
-    public int calculate(int current) {
-
-        int result = current + plus + minus;
-        if (result < 0) {
-            // todo: 보유 금액 관련 커스텀 예외 필요
-            throw new IllegalStateException("보유 금액이 부족합니다.");
-        }
-
-        return result;
     }
 }
