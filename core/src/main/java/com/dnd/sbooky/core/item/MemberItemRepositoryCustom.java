d72151e54ac73b23bd9f3bc3952197ae292bcbb5
hwangdaesun <hds650588@gmail.com>
2025-02-12 03:24:15 +0900

    feat : 회원이 소유한 특정 회원_아이템을 조회하는 쿼리 구현

--- core/src/main/java/com/dnd/sbooky/core/item/MemberItemRepositoryCustom.java
+++ core/src/main/java/com/dnd/sbooky/core/item/MemberItemRepositoryCustom.java
@@ -2,6 +2,7 @@ package com.dnd.sbooky.core.item;
 
 import com.dnd.sbooky.core.item.dto.FindItemDTO;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
@@ -9,6 +10,8 @@ public interface MemberItemRepositoryCustom {
     List<FindItemDTO> findItemsByMemberId(Long memberId);
 
     List<FindItemDTO> findEquippedItemsByMemberId(Long memberId);
+
+    Optional<MemberItemEntity> findMemberItemByMemberIdAndItemId(Long memberId, Long itemId);
 
     boolean existsByMemberIdAndItemId(Long memberId, Long itemId);
 }
