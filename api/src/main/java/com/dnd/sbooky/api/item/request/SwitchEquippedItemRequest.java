9c3504fd4bc1b26e3c66967976e994b00be1a6c3
hwangdaesun <hds650588@gmail.com>
2025-02-12 23:52:45 +0900

    style : spotless 적용

--- api/src/main/java/com/dnd/sbooky/api/item/request/SwitchEquippedItemRequest.java
+++ api/src/main/java/com/dnd/sbooky/api/item/request/SwitchEquippedItemRequest.java
@@ -2,6 +2,7 @@ package com.dnd.sbooky.api.item.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
+
 @Schema(description = "아이템 착용 요청 DTO")
 public record SwitchEquippedItemRequest(
         @Schema(description = "기존 착용하고 있는 아이템") @NotNull Long equippedItemId,
