a0060664e02d189dca0f811e0e52fb53a94e0e37
hwangdaesun <hds650588@gmail.com>
2025-02-14 01:26:31 +0900

    fix : domain 주석 처리

--- api/src/main/java/com/dnd/sbooky/api/security/OAuth2SuccessHandler.java
+++ api/src/main/java/com/dnd/sbooky/api/security/OAuth2SuccessHandler.java
@@ -49,7 +49,7 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
                         .sameSite(SameSite.NONE.getValue())
                         .httpOnly(true)
                         .maxAge(REFRESH_TOKEN_EXPIRE_TIME)
-                        .domain(".sbooky.net")
+//                        .domain(".sbooky.net")
                         .path("/")
                         .build();
         response.addHeader(SET_COOKIE, cookie.toString());
