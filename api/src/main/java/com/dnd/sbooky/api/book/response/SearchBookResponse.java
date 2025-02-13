4efcf62d18f12d0d0a63dae073678a96fbca3258
seungjo <f1v3@kakao.com>
2025-02-14 02:18:57 +0900

    Revert "fix : 응답에서 작가 1명만 응답하는 구조로 수정"
    This reverts commit 4e5431d96ce45825f344256495f9a2e4a66d86d1.

--- api/src/main/java/com/dnd/sbooky/api/book/response/SearchBookResponse.java
+++ api/src/main/java/com/dnd/sbooky/api/book/response/SearchBookResponse.java
@@ -9,17 +9,17 @@ import java.util.List;
 @Schema(name = "SearchBookResponse", description = "책 검색 결과")
 public record SearchBookResponse(
         // spotless:off
-        @Schema(description = "검색된 책 목록")
+        @Schema(name = "책 목록", description = "검색된 책 목록")
         List<Book> books,
 
-        @Schema(description = "페이지 정보")
+        @Schema(name = "페이지 정보", description = "페이지 정보")
         PageInfo pageInfo) {
 
     public static SearchBookResponse from(KakaoSearchBookResponseDTO dto) {
         List<Book> books = dto.documents().stream()
                               .map(document -> new Book(
                                       document.title(),
-                                      document.authors().getFirst(),
+                                      document.authors(),
                                       document.datetime() == null
                                               ? LocalDate.EPOCH : document.datetime().toLocalDate(),
                                       document.extractThumbnailFileName())
@@ -32,19 +32,17 @@ public record SearchBookResponse(
     }
 
     public record Book(
-            @Schema(description = "책 제목") String title,
-            @Schema(description = "저자") String author,
-            @Schema(description = "출판일") LocalDate publishedAt,
-            @Schema(description = "썸네일 URL") String thumbnail) {
+            @Schema(name = "책 제목") String title,
+            @Schema(name = "저자 리스트") List<String> authors,
+            @Schema(name = "출판일") LocalDate publishedAt,
+            @Schema(name = "썸네일 URL") String thumbnail) {
 
     }
 
     public record PageInfo(
-            @Schema(description = "마지막 페이지") boolean isEnd,
-            @Schema(description = "중복된 책 제외 노출 가능 책 수") int pageableCount,
-            @Schema(description = "검색된 책의 수") int totalCount) {
-
-    }
+            @Schema(name = "마지막 페이지") boolean isEnd,
+            @Schema(name = "중복된 책 제외 노출 가능 책 수") int pageableCount,
+            @Schema(name = "검색된 책의 수") int totalCount) {}
     // spotless:on
 
 }
