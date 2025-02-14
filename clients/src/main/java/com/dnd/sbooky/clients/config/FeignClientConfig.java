962bb6516c4730a11293666ea6f8e3832e143db0
seungjo <f1v3@kakao.com>
2025-02-14 17:27:11 +0900

    feat : 타임아웃 설정

--- clients/src/main/java/com/dnd/sbooky/clients/config/FeignClientConfig.java
+++ clients/src/main/java/com/dnd/sbooky/clients/config/FeignClientConfig.java
@@ -1,8 +1,27 @@
 package com.dnd.sbooky.clients.config;
 
+import feign.Request;
+import java.time.Duration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableFeignClients(basePackages = "com.dnd.sbooky.clients")
-class FeignClientConfig {}
+class FeignClientConfig {
+
+    private static final long CONNECT_TIMEOUT_MILLIS = 5000L;
+    private static final long READ_TIMEOUT_MILLIS = 5000L;
+
+    /**
+     * Timeout Setting
+     */
+    @Bean
+    Request.Options feignOptions() {
+        return new Request.Options(
+                Duration.ofMillis(CONNECT_TIMEOUT_MILLIS),
+                Duration.ofMillis(READ_TIMEOUT_MILLIS),
+                true
+        );
+    }
+}
