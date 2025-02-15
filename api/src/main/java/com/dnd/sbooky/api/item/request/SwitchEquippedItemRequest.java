17532308906b70566a478ef5d46d2741a4749a1b
hwangdaesun <hds650588@gmail.com>
2025-02-15 11:37:15 +0900

    fix : code로 통신하도록 수정

--- api/src/main/java/com/dnd/sbooky/api/item/request/SwitchEquippedItemRequest.java
+++ api/src/main/java/com/dnd/sbooky/api/item/request/SwitchEquippedItemRequest.java
@@ -5,5 +5,5 @@ import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "아이템 착용 요청 DTO")
 public record SwitchEquippedItemRequest(
-        @Schema(description = "기존 착용하고 있는 아이템") @NotNull Long equippedItemId,
-        @Schema(description = "앞으로 착용할 아이템") @NotNull Long toEquipItemId) {}
+        @Schema(description = "기존 착용하고 있는 아이템") @NotNull String equippedItemCode,
+        @Schema(description = "앞으로 착용할 아이템") @NotNull String toEquipItemCode) {}
