f047d3e2160a563968fb64825ec3fec76765e4f8
seungjo <f1v3@kakao.com>
2025-02-16 15:41:52 +0900

    feat : FeignClient 재시도 설정

--- clients/src/main/java/com/dnd/sbooky/clients/config/FeignClientConfig.java
+++ clients/src/main/java/com/dnd/sbooky/clients/config/FeignClientConfig.java
@@ -1,6 +1,7 @@
 package com.dnd.sbooky.clients.config;
 
 import feign.Request;
+import feign.Retryer;
 import java.time.Duration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,10 @@ class FeignClientConfig {
     private static final long CONNECT_TIMEOUT_MILLIS = 5000L;
     private static final long READ_TIMEOUT_MILLIS = 5000L;
 
+    private static final long RETRY_PERIOD = 100L;
+    private static final long RETRY_MAX_PERIOD = 2000L;
+    private static final int RETRY_MAX_ATTEMPTS = 3;
+
     /**
      * Timeout Setting
      */
@@ -23,5 +28,14 @@ class FeignClientConfig {
                 Duration.ofMillis(READ_TIMEOUT_MILLIS),
                 true
         );
+    }
+
+
+    /**
+     * Retry Setting
+     */
+    @Bean
+    Retryer.Default feignRetryer() {
+        return new Retryer.Default(RETRY_PERIOD, RETRY_MAX_PERIOD, RETRY_MAX_ATTEMPTS);
     }
 }
