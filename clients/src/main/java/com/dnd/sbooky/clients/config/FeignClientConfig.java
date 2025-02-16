b69569f20f6511f0157f9bec7643b8141c2f5d91
seungjo <f1v3@kakao.com>
2025-02-16 21:46:22 +0900

    feat : 로컬환경 로깅 추가

--- clients/src/main/java/com/dnd/sbooky/clients/config/FeignClientConfig.java
+++ clients/src/main/java/com/dnd/sbooky/clients/config/FeignClientConfig.java
@@ -1,11 +1,13 @@
 package com.dnd.sbooky.clients.config;
 
+import feign.Logger;
 import feign.Request;
 import feign.Retryer;
 import java.time.Duration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @EnableFeignClients(basePackages = "com.dnd.sbooky.clients")
@@ -38,4 +40,14 @@ class FeignClientConfig {
     Retryer.Default feignRetryer() {
         return new Retryer.Default(RETRY_PERIOD, RETRY_MAX_PERIOD, RETRY_MAX_ATTEMPTS);
     }
+
+    /**
+     * Logging Setting
+     */
+    @Profile("local")
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.BASIC;
+    }
+
 }
