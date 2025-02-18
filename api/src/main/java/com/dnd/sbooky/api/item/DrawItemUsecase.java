04dee696f77dc0689f080f0357b6d620473bc633
hwangdaesun <hds650588@gmail.com>
2025-02-18 21:00:27 +0900

    refactor : ItemCode를 사용해서 ItemRandomFetcher 클래스 수정

--- api/src/main/java/com/dnd/sbooky/api/item/DrawItemUsecase.java
+++ api/src/main/java/com/dnd/sbooky/api/item/DrawItemUsecase.java
@@ -15,7 +15,6 @@ import com.dnd.sbooky.core.item.MemberItemRepository;
 import com.dnd.sbooky.core.member.MemberEntity;
 import com.dnd.sbooky.core.member.MemberRepository;
 import com.dnd.sbooky.core.point.PointPolicy;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,6 +27,7 @@ public class DrawItemUsecase {
     private final ItemRepository itemRepository;
     private final MemberRepository memberRepository;
     private final MemberItemRepository memberItemRepository;
+    private final ItemRandomFetcher itemRandomFetcher;
 
     @Transactional
     public DrawItemResponse drawItem(Long memberId) {
@@ -54,8 +54,6 @@ public class DrawItemUsecase {
     }
 
     private Long getRandomItemId() {
-        List<Long> itemIds = itemRepository.findAllItemIds();
-        ItemRandomFetcher itemRandomFetcher = new ItemRandomFetcher(itemIds);
         Long randomItemId = itemRandomFetcher.generateRandomItem();
         return randomItemId;
     }
--- core/src/main/java/com/dnd/sbooky/core/item/ItemRandomFetcher.java
+++ core/src/main/java/com/dnd/sbooky/core/item/ItemRandomFetcher.java
@@ -1,16 +1,17 @@
 package com.dnd.sbooky.core.item;
 
-import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ItemRandomFetcher {
-    private final List<Long> itemIds;
+    private final long ITEM_SIZE;
 
-    public ItemRandomFetcher(List<Long> itemIds) {
-        this.itemIds = itemIds;
+    public ItemRandomFetcher() {
+        this.ITEM_SIZE = ItemCode.values().length;
     }
 
     public Long generateRandomItem() {
-        return ThreadLocalRandom.current().nextLong(1, itemIds.size() + 1);
+        return ThreadLocalRandom.current().nextLong(1, ITEM_SIZE + 1);
     }
 }
