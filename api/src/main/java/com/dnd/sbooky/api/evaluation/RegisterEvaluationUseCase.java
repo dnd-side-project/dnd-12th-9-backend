b25b91812c2331050253aaf08f650f6ed88dc87e
seungjo <f1v3@kakao.com>
2025-02-19 00:49:18 +0900

    feat : 첫 평가시 포인트 지급 로직 추가

--- api/src/main/java/com/dnd/sbooky/api/evaluation/RegisterEvaluationUseCase.java
+++ api/src/main/java/com/dnd/sbooky/api/evaluation/RegisterEvaluationUseCase.java
@@ -5,6 +5,7 @@ import com.dnd.sbooky.api.book.exception.BookNotFoundException;
 import com.dnd.sbooky.api.book.exception.BookReadStatusException;
 import com.dnd.sbooky.api.evaluation.exception.EvaluationNotFoundException;
 import com.dnd.sbooky.api.evaluation.request.RegisterEvaluationRequest;
+import com.dnd.sbooky.api.point.AccumulatePointUseCase;
 import com.dnd.sbooky.api.support.error.ErrorType;
 import com.dnd.sbooky.core.book.MemberBookEntity;
 import com.dnd.sbooky.core.book.MemberBookRepository;
@@ -13,6 +14,7 @@ import com.dnd.sbooky.core.evaluation.BookEvaluationEntity;
 import com.dnd.sbooky.core.evaluation.BookEvaluationRepository;
 import com.dnd.sbooky.core.evaluation.EvaluationEntity;
 import com.dnd.sbooky.core.evaluation.EvaluationRepository;
+import com.dnd.sbooky.core.point.PointPolicy;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -26,11 +28,13 @@ public class RegisterEvaluationUseCase {
     private final MemberBookRepository memberBookRepository;
     private final BookEvaluationRepository bookEvaluationRepository;
     private final EvaluationRepository evaluationRepository;
+    private final AccumulatePointUseCase accumulatePointUseCase;
 
     public void register(Long memberBookId, Long memberId, RegisterEvaluationRequest request) {
 
         MemberBookEntity memberBook = validateAndGetMemberBook(memberBookId, memberId);
 
+        boolean hasEvaluation = bookEvaluationRepository.existsByMemberBookId(memberBookId);
         bookEvaluationRepository.deleteAllByMemberBookId(memberBookId);
 
         List<EvaluationEntity> evaluations = getValidatedEvaluations(request.keywordIds());
@@ -41,6 +45,11 @@ public class RegisterEvaluationUseCase {
                         .toList();
 
         bookEvaluationRepository.saveAll(bookEvaluations);
+
+        if (!hasEvaluation) {
+            accumulatePointUseCase.accumulate(
+                    memberBook.getMemberEntity(), PointPolicy.COMPLETE_BOOK_EVALUATION);
+        }
     }
 
     private MemberBookEntity validateAndGetMemberBook(Long memberBookId, Long memberId) {
