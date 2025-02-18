9a755b4e8e3e841d9e47b2b30764cecbfb74c79a
hwangdaesun <hds650588@gmail.com>
2025-02-18 18:48:03 +0900

    feat : 모든 아이템 PK를 조회하는 메서드 구현

--- core/src/main/java/com/dnd/sbooky/core/item/ItemRepository.java
+++ core/src/main/java/com/dnd/sbooky/core/item/ItemRepository.java
@@ -1,5 +1,11 @@
 package com.dnd.sbooky.core.item;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface ItemRepository extends JpaRepository<ItemEntity, Long> {}
+public interface ItemRepository extends JpaRepository<ItemEntity, Long> {
+
+    @Query("select i.id from ItemEntity i")
+    List<Long> findAllItemIds();
+}
