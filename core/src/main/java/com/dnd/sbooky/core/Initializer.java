198399877999c68a15d539f8b75f3706fef58728
hwangdaesun <hds650588@gmail.com>
2025-02-18 18:46:57 +0900

    feat : 추가된 아이템 반영

--- core/src/main/java/com/dnd/sbooky/core/Initializer.java
+++ core/src/main/java/com/dnd/sbooky/core/Initializer.java
@@ -30,6 +30,15 @@ public class Initializer {
         likeRepository.save(LikeEntity.newInstance(memberEntity.getId()));
         itemRepository.save(ItemEntity.newInstance(1L, ItemType.CHARACTER, "떠돌이 유령"));
         itemRepository.save(ItemEntity.newInstance(2L, ItemType.CHARACTER, "유령"));
+        itemRepository.save(ItemEntity.newInstance(3L, ItemType.CHARACTER, "고양이 유령"));
+        itemRepository.save(ItemEntity.newInstance(4L, ItemType.CHARACTER, "마법사 유령"));
+        itemRepository.save(ItemEntity.newInstance(5L, ItemType.CHARACTER, "명탐정 유령"));
+        itemRepository.save(ItemEntity.newInstance(6L, ItemType.CHARACTER, "어린왕자 유령"));
+        itemRepository.save(ItemEntity.newInstance(7L, ItemType.CHARACTER, "프랑켄슈타인 유령"));
+        itemRepository.save(ItemEntity.newInstance(8L, ItemType.CHARACTER, "빨간망토 유령"));
+        itemRepository.save(ItemEntity.newInstance(9L, ItemType.CHARACTER, "치즈 고양이 유령"));
+        itemRepository.save(ItemEntity.newInstance(10L, ItemType.CHARACTER, "샴 고양이 유령"));
+        itemRepository.save(ItemEntity.newInstance(11L, ItemType.CHARACTER, "백도 고양이 유령"));
 
         Arrays.stream(EvaluationKeyword.values())
                 .forEach(keyword -> evaluationRepository.save(EvaluationEntity.newInstance(keyword)));
