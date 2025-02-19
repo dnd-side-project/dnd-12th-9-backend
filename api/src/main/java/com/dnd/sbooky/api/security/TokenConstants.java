1ad67d652120cd446ca74e90d6e22c3d80c1b133
seungjo <f1v3@kakao.com>
2025-02-20 00:28:17 +0900

    fix : AT 만료시간 7일로 임시 변경

--- api/src/main/java/com/dnd/sbooky/api/security/TokenConstants.java
+++ api/src/main/java/com/dnd/sbooky/api/security/TokenConstants.java
@@ -4,7 +4,9 @@ public class TokenConstants {
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String REFRESH_TOKEN = "refreshToken";
 
-    public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30L; // 30 minutes
+    // fixme : ACCESS TOKEN EXPIRE TIME 변경해야함!!
+//    public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30L; // 30 minutes
+    public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7L; // 임시로 7일 설정
 
     public static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 14L; // 14 days
 
