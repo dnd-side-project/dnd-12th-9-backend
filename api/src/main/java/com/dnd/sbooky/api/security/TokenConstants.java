796beaf7662e50124bea16ee3e716563af9c6f85
seungjo <f1v3@kakao.com>
2025-02-20 00:30:07 +0900

    chore : code formatter 적용

--- api/src/main/java/com/dnd/sbooky/api/security/TokenConstants.java
+++ api/src/main/java/com/dnd/sbooky/api/security/TokenConstants.java
@@ -5,7 +5,7 @@ public class TokenConstants {
     public static final String REFRESH_TOKEN = "refreshToken";
 
     // fixme : ACCESS TOKEN EXPIRE TIME 변경해야함!!
-//    public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30L; // 30 minutes
+    //    public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30L; // 30 minutes
     public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7L; // 임시로 7일 설정
 
     public static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 14L; // 14 days
