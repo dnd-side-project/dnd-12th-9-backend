447a13d5ae2c8368808609e76f25a8a35fbb977c
seungjo <f1v3@kakao.com>
2025-04-05 01:04:52 +0900

    feat : 프론트 리다이렉트 정보 관리

---
+++ api/src/main/java/com/dnd/sbooky/api/security/RedirectProperties.java
@@ -0,0 +1,19 @@
+package com.dnd.sbooky.api.security;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "redirect")
+public class RedirectProperties {
+
+    private String local;
+    private String prod;
+    private String dev;
+
+}
