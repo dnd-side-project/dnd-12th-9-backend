d7f6da3a8dae171ad0f752b30a7f6b46ee41fabf
hwangdaesun <hds650588@gmail.com>
2025-04-10 21:48:00 +0900

    feat : @ConfigurationPropertiesScan 추가

--- api/src/main/java/com/dnd/sbooky/SbookyApplication.java
+++ api/src/main/java/com/dnd/sbooky/SbookyApplication.java
@@ -2,8 +2,10 @@ package com.dnd.sbooky;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class SbookyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SbookyApplication.class, args);
