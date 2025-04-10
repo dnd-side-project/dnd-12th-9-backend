064d9c02dca747a0856c4f21608e0ea6bbdf468d
hwangdaesun <hds650588@gmail.com>
2025-04-10 19:38:22 +0900

    feat : ApiSpec과 Controller에 FindMemberEquippedItemsResponseV2 반영

--- api/src/main/java/com/dnd/sbooky/api/docs/spec/FindItemsV2ApiSpec.java
+++ api/src/main/java/com/dnd/sbooky/api/docs/spec/FindItemsV2ApiSpec.java
@@ -1,6 +1,6 @@
 package com.dnd.sbooky.api.docs.spec;
 
-import com.dnd.sbooky.api.item.v1.response.FindMemberEquippedItemsResponse;
+import com.dnd.sbooky.api.item.v2.response.FindMemberEquippedItemsResponseV2;
 import com.dnd.sbooky.api.support.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -8,5 +8,5 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "[Item API v2]", description = "아이템 관련된 API v2")
 public interface FindItemsV2ApiSpec {
     @Operation(summary = "내가 장착한 아이템 조회", description = "내가 장착한 아이템 정보를 조회한다.")
-    ApiResponse<FindMemberEquippedItemsResponse> findEquippedItems(Long ownerId);
+    ApiResponse<FindMemberEquippedItemsResponseV2> findEquippedItems(Long ownerId);
 }
--- api/src/main/java/com/dnd/sbooky/api/item/v2/FindItemsControllerV2.java
+++ api/src/main/java/com/dnd/sbooky/api/item/v2/FindItemsControllerV2.java
@@ -3,7 +3,7 @@ package com.dnd.sbooky.api.item.v2;
 import com.dnd.sbooky.api.docs.spec.FindItemsV2ApiSpec;
 import com.dnd.sbooky.api.item.v1.FindEquippedItemUsecase;
 import com.dnd.sbooky.api.item.v1.response.FindEquippedItemsResponse;
-import com.dnd.sbooky.api.item.v1.response.FindMemberEquippedItemsResponse;
+import com.dnd.sbooky.api.item.v2.response.FindMemberEquippedItemsResponseV2;
 import com.dnd.sbooky.api.member.FindNicknameUsecase;
 import com.dnd.sbooky.api.support.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
@@ -21,10 +21,10 @@ public class FindItemsControllerV2 implements FindItemsV2ApiSpec {
     private final FindNicknameUsecase findNicknameUsecase;
 
     @GetMapping("/members/{ownerId}/items/equipped")
-    public ApiResponse<FindMemberEquippedItemsResponse> findEquippedItems(
+    public ApiResponse<FindMemberEquippedItemsResponseV2> findEquippedItems(
             @PathVariable Long ownerId) {
         FindEquippedItemsResponse equippedItemsDTO = findEquippedItemUsecase.findEquippedItems(ownerId);
         String nickname = findNicknameUsecase.findNickname(ownerId);
-        return ApiResponse.success(FindMemberEquippedItemsResponse.from(equippedItemsDTO, nickname));
+        return ApiResponse.success(FindMemberEquippedItemsResponseV2.from(equippedItemsDTO, nickname));
     }
 }
