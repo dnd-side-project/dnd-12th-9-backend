d21231ac09d1ea32febd3d1ba887287a6e9dea3a
hwangdaesun <hds650588@gmail.com>
2025-04-10 19:31:20 +0900

    feat : 내가 장착한 아이템 조회 API v2 controller 추가

---
+++ api/src/main/java/com/dnd/sbooky/api/item/v2/FindItemsControllerV2.java
@@ -0,0 +1,30 @@
+package com.dnd.sbooky.api.item.v2;
+
+import com.dnd.sbooky.api.docs.spec.FindItemsV2ApiSpec;
+import com.dnd.sbooky.api.item.v1.FindEquippedItemUsecase;
+import com.dnd.sbooky.api.item.v1.response.FindEquippedItemsResponse;
+import com.dnd.sbooky.api.item.v1.response.FindMemberEquippedItemsResponse;
+import com.dnd.sbooky.api.member.FindNicknameUsecase;
+import com.dnd.sbooky.api.support.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class FindItemsControllerV2 implements FindItemsV2ApiSpec {
+
+    private final FindEquippedItemUsecase findEquippedItemUsecase;
+    private final FindNicknameUsecase findNicknameUsecase;
+
+    @GetMapping("/members/{ownerId}/items/equipped")
+    public ApiResponse<FindMemberEquippedItemsResponse> findEquippedItems(
+            @PathVariable Long ownerId) {
+        FindEquippedItemsResponse equippedItemsDTO = findEquippedItemUsecase.findEquippedItems(ownerId);
+        String nickname = findNicknameUsecase.findNickname(ownerId);
+        return ApiResponse.success(FindMemberEquippedItemsResponse.from(equippedItemsDTO, nickname));
+    }
+}
