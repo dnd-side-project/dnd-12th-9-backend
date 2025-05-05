1b98c7783ba57198b6e2ccb0bd8b190a469dc924
seungjo <f1v3@kakao.com>
2025-05-05 22:30:21 +0900

    feat : 책 검색 로직 캐시 도입

--- api/src/main/java/com/dnd/sbooky/api/book/v1/SearchBookUseCase.java
+++ api/src/main/java/com/dnd/sbooky/api/book/v1/SearchBookUseCase.java
@@ -1,18 +1,52 @@
 package com.dnd.sbooky.api.book.v1;
 
 import com.dnd.sbooky.api.book.v1.response.SearchBookResponse;
+import com.dnd.sbooky.api.support.cache.CustomCacheManager;
 import com.dnd.sbooky.clients.api.BookSearchAdapter;
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
-@RequiredArgsConstructor
 public class SearchBookUseCase {
 
     private final BookSearchAdapter bookSearchAdapter;
+    private final CustomCacheManager<SearchBookResponse> cacheManager;
+
+    public SearchBookUseCase(
+            BookSearchAdapter bookSearchAdapter,
+            @Qualifier("caffeineCacheManager") CustomCacheManager<SearchBookResponse> cacheManager) {
+        this.bookSearchAdapter = bookSearchAdapter;
+        this.cacheManager = cacheManager;
+    }
 
     public SearchBookResponse search(String query, int page) {
 
-        return SearchBookResponse.from(bookSearchAdapter.search(query, page));
+        String cacheKey = query + ":" + page;
+
+        return cacheManager
+                .getFromCache(cacheKey)
+                .map(
+                        response -> {
+                            log.debug("[Cache hit] key: {}", cacheKey);
+                            return response;
+                        })
+                .orElseGet(
+                        () -> {
+                            log.debug("[Cache miss] key: {}", cacheKey);
+                            SearchBookResponse response =
+                                    SearchBookResponse.from(bookSearchAdapter.search(query, page));
+
+                            if (isCacheable(response)) {
+                                cacheManager.addToCache(cacheKey, response);
+                                log.debug("[Cache add] key: {}", cacheKey);
+                            }
+                            return response;
+                        });
+    }
+
+    private boolean isCacheable(SearchBookResponse response) {
+        return response.books() != null && !response.books().isEmpty();
     }
 }
