7667297d0dbba29829de9ad1ea96c1bbdf91db83
seungjo <f1v3@kakao.com>
2025-05-05 21:09:48 +0900

    chore : spotless 적용

--- api/src/main/java/com/dnd/sbooky/api/support/cache/LinkedHashMapCacheManager.java
+++ api/src/main/java/com/dnd/sbooky/api/support/cache/LinkedHashMapCacheManager.java
@@ -1,7 +1,6 @@
 package com.dnd.sbooky.api.support.cache;
 
 import com.dnd.sbooky.api.book.v1.response.SearchBookResponse;
-import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
@@ -18,22 +17,20 @@ public class LinkedHashMapCacheManager implements CustomCacheManager<SearchBookR
 
     private static final int MAX_CACHE_SIZE = 10;
 
-
-    private final Map<String, SearchBookResponse> cache = new LinkedHashMap<>(
-            MAX_CACHE_SIZE,
-            0.75f,
-            true
-    ) {
-        @Override
-        protected boolean removeEldestEntry(Map.Entry<String, SearchBookResponse> eldest) {
-            boolean shouldRemove = size() > MAX_CACHE_SIZE;
-            if (shouldRemove) {
-                log.info("CacheManager - Cache size exceeded. Removing eldest entry: key={}, value={}",
-                        eldest.getKey(), eldest.getValue());
-            }
-            return shouldRemove;
-        }
-    };
+    private final Map<String, SearchBookResponse> cache =
+            new LinkedHashMap<>(MAX_CACHE_SIZE, 0.75f, true) {
+                @Override
+                protected boolean removeEldestEntry(Map.Entry<String, SearchBookResponse> eldest) {
+                    boolean shouldRemove = size() > MAX_CACHE_SIZE;
+                    if (shouldRemove) {
+                        log.info(
+                                "CacheManager - Cache size exceeded. Removing eldest entry: key={}, value={}",
+                                eldest.getKey(),
+                                eldest.getValue());
+                    }
+                    return shouldRemove;
+                }
+            };
 
     @Override
     public void addToCache(String key, SearchBookResponse value) {
