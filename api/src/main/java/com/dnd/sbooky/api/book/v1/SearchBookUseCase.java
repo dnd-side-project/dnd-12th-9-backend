2cd7fd3254c57161a4cfdd92e641f0e6f30a7b04
seungjo <f1v3@kakao.com>
2025-05-06 11:42:54 +0900

    refactor : @Qualifier 제거

--- api/src/main/java/com/dnd/sbooky/api/book/v1/SearchBookUseCase.java
+++ api/src/main/java/com/dnd/sbooky/api/book/v1/SearchBookUseCase.java
@@ -3,23 +3,17 @@ package com.dnd.sbooky.api.book.v1;
 import com.dnd.sbooky.api.book.v1.response.SearchBookResponse;
 import com.dnd.sbooky.api.support.cache.CustomCacheManager;
 import com.dnd.sbooky.clients.api.BookSearchAdapter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class SearchBookUseCase {
 
     private final BookSearchAdapter bookSearchAdapter;
     private final CustomCacheManager<SearchBookResponse> cacheManager;
-
-    public SearchBookUseCase(
-            BookSearchAdapter bookSearchAdapter,
-            @Qualifier("caffeineCacheManager") CustomCacheManager<SearchBookResponse> cacheManager) {
-        this.bookSearchAdapter = bookSearchAdapter;
-        this.cacheManager = cacheManager;
-    }
 
     public SearchBookResponse search(String query, int page) {
 
