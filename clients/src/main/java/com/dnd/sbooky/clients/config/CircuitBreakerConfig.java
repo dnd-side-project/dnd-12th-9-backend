9a81064c50d47862ce49ea849f28d3674a121b3f
seungjo <f1v3@kakao.com>
2025-08-29 14:07:22 +0900

    remove: 불필요한 클래스 제거

--- clients/src/main/java/com/dnd/sbooky/clients/config/CircuitBreakerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.dnd.sbooky.clients.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class CircuitBreakerConfig {
-
-    public static final String CIRCUIT_REDIS = "CIRCUIT_REDIS";
-}
